b810593db10e322c3c1906ac2ab829e7beb8a694
devtrills <godwinikechukwu.dev@gmail.com>
2023-11-16 04:41:22 +0100

    Update MySwitch.tsx

--- src/components/MySwitch.tsx
+++ src/components/MySwitch.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { FC, useEffect, useState } from "react";
 import { Switch } from "@headlessui/react";
+
 import Label from "./Label/Label";
 
 export interface MySwitchProps {
